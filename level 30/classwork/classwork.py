43010afdb6c38c3d671dec168d274491e84a8833
gigo04 <gtatishvili26@gmail.com>
2024-12-24 23:42:26 +0400

    second commit

---
+++ level 30/classwork/classwork.py
@@ -0,0 +1,28 @@
+def min_and_max_num(num):
+    num = list(int, num.split())
+    highest = max(num)
+    lowest = min(num)
+    return f"({highest} {lowest})"
+print(min_and_max_num("1 2 5 10"))
+
+
+def find_vowel(x):
+    vowel = ("a e i o u")
+    if x in  vowel:
+        return ("yochag")
+    else:
+        return ("ar yochag")
+    print(find_vowel(a))
+
+
+
+
+
+
+
+
+
+
+
+    def min_and_max_num(num):
+        num = 
