4b20b5e91226df2075a4d6043db073b0a58247ec
gigo04 <gtatishvili26@gmail.com>
2025-02-10 19:27:41 +0400

    second commit

---
+++ level 6/home/2.py
@@ -0,0 +1,6 @@
+length = float(input("შემოიტანეთ ოთკუთხედის სიგრძე: "))
+width = float(input("შემოიტანეთ ოთკუთხედის სიგანე: "))
+area = length * width
+perimeter = 2 * (length + width)
+print(f"ოთკუთხედის ფართობია: {area}")
+print(f"ოთკუთხედის პერიმეტრია: {perimeter}")
---
+++ level 6/home/3.py
@@ -0,0 +1,7 @@
+distance_km = float(input("შემოიტანეთ სახლიდან სკოლამდე მანძილი კილომეტრებში: "))
+distance_meters = distance_km * 1000
+distance_centimeters = distance_km * 100000
+distance_millimeters = distance_km * 1000000
+print(f"მანძილი სახლიდან სკოლამდე მეტრებში: {distance_meters}")
+print(f"მანძილი სახლიდან სკოლამდე სანტიმეტრებში: {distance_centimeters}")
+print(f"მანძილი სახლიდან სკოლამდე მილიმეტრებში: {distance_millimeters}")
